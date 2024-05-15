fc34d16d8cd123ff02bb2bd64fdbbb2f2b8c5ef9
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-05-16 00:18:57 +0200

    Update Missile.py

--- Normal Builds/Missile.py
+++ Normal Builds/Missile.py
@@ -5,18 +5,18 @@ counter = 0
 def on_on_chat():
     startpos = player.position()
     loops.pause(1000)
-    global counter
+    global buildingblock    
     radius = 4
     for i in range(4):
         shapes.circle(buildingblock, startpos, radius, Axis.X, ShapeOperation.REPLACE)
         startpos = positions.add(startpos, pos(1, 0, 0))
         setbuildingblock()
         radius += 1
+    buildingblock = IRON_BLOCK
     for f in range(fibo.length):
         for i in range(fibo[fibo.length-f]):
             shapes.circle(buildingblock, startpos, radius, Axis.X, ShapeOperation.REPLACE)
             startpos = positions.add(startpos, pos(1, 0, 0))
-            setbuildingblock()
         radius -= 1
     for i in range(8):
         blocks.fill(buildingblock, positions.add(startpos, pos(0, -8-radius+i, 0)), positions.add(startpos, pos(0, 8+radius-i, 0)))
@@ -31,7 +31,6 @@ def setbuildingblock():
     global counter
     global buildingblock
     counter += 1
-    player.say(counter)
     if(counter%2 == 0):
         buildingblock = IRON_BLOCK
     else:
