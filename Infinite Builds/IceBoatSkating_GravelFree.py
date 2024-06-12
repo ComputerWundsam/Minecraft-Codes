36f98bceb00868905a8d999ec9789d54e91656f1
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-06-12 23:09:42 +0200

    Create IceBoatSkating_GravelFree.py

---
+++ Infinite Builds/IceBoatSkating_GravelFree.py
@@ -0,0 +1,11 @@
+def on_on_chat():
+    startpos = positions.add(player.position(), pos(0, -1, -7))
+    blocks.fill(ICE, startpos, positions.add(startpos, pos(20, 0, 20)))
+    startpos = positions.add(startpos, pos(-15, 0, randint(-2, 2)))
+    while True:
+        blocks.fill(AIR, startpos, positions.add(startpos, pos(20, 6, 20)))
+        blocks.fill(GLASS, positions.add(startpos, pos(0, 7, 0)), 
+            positions.add(startpos, pos(20, 7, 20)))
+        blocks.fill(ICE, startpos, positions.add(startpos, pos(20, 0, 20)))
+        startpos = positions.add(startpos, pos(3, 0, randint(-2, 2)))
+player.on_chat("run", on_on_chat)
