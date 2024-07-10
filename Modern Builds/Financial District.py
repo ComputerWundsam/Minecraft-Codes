088aaf285224677ed71da4f0bb09579f802bc81c
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-07-10 11:14:54 +0200

    Update Financial District.py

--- Modern Builds/Financial District.py
+++ Modern Builds/Financial District.py
@@ -1,12 +1,13 @@
 streetblock = BLACK_CONCRETE
 whiteblock = WHITE_CONCRETE
+pavementblock = STONE_BRICKS_SLAB
 glassblock = GLASS
-houseblocks = BLUE_CONCRETE, GREEN_CONCRETE, BRICKS, STONE_BRICKS
-streetwidth = 12
+houseblocks = [BLUE_CONCRETE, GREEN_CONCRETE, RED_CONCRETE, BROWN_CONCRETE, IRON_BLOCK, WHITE_CONCRETE, CONCRETE, BLACK_CONCRETE]
+streetwidth = 16
 startpos = pos(0, 0, 0)
 streetlength = 0
 
-def on_on_chat():
+def createdistrict():
     global startpos, streetlength
     startpos = positions.add(player.position(), pos(0, -1, 0))
     while True:
@@ -14,24 +15,38 @@ def on_on_chat():
         streetlength = randint(30, 20)
         while counter < streetlength:
             createstreet(startpos)
+            if(counter == 0):
+                blocks.fill(whiteblock, startpos, positions.add(startpos, pos(0, 0, streetwidth)))
             startpos = positions.add(startpos, pos(1, 0, 0))
             counter += 1
-        createhouse(startpos, streetlength)
-        createhouse(positions.add(startpos, pos(0, 0, streetwidth+streetlength+1)), streetlength)
-
+        createhouse(positions.add(startpos, pos(1, 0, 0)), streetlength)
+        createhouse(positions.add(startpos, pos(1, 0, streetwidth+streetlength+1)), streetlength)
         createcrossroads()
 
 def createstreet(setpos):
     blocks.fill(streetblock, setpos, positions.add(setpos, pos(0, 0, streetwidth)))
     blocks.place(whiteblock, positions.add(setpos, pos(0, 0, streetwidth/2)))
+    blocks.place(pavementblock, positions.add(setpos, pos(0, 1, streetwidth)))
+    blocks.place(pavementblock, positions.add(setpos, pos(0, 1, 0)))
 
 def createcrossroads():
     global startpos
     blocks.fill(whiteblock, startpos, positions.add(startpos, pos(-1, 0, streetwidth)))
     for i in range(streetwidth):
         blocks.fill(streetblock, positions.add(startpos, pos(0, 0, (i-streetwidth/2)*streetwidth+streetwidth)), positions.add(startpos, pos(streetwidth, 0, ((i-streetwidth/2)*streetwidth))))
+        if(i-streetwidth/2 != 0):
+            if(i-streetwidth/2 == 1):
+                blocks.fill(whiteblock, positions.add(startpos, pos(streetwidth, 0, streetwidth)), positions.add(startpos, pos(0, 0, streetwidth)))
+            blocks.fill(whiteblock, positions.add(startpos, pos(streetwidth/2, 0, (i-streetwidth/2)*streetwidth+streetwidth)), positions.add(startpos, pos(streetwidth/2, 0, ((i-streetwidth/2)*streetwidth))))
+            blocks.fill(pavementblock, positions.add(startpos, pos(streetwidth, 1, (i-streetwidth/2)*streetwidth+streetwidth)), positions.add(startpos, pos(streetwidth, 1, ((i-streetwidth/2)*streetwidth))))            
+            blocks.fill(pavementblock, positions.add(startpos, pos(0, 1, (i-streetwidth/2)*streetwidth+streetwidth)), positions.add(startpos, pos(0, 1, ((i-streetwidth/2)*streetwidth))))
+        else:
+            blocks.fill(whiteblock, positions.add(startpos, pos(streetwidth, 0, 0)), startpos)        
+    blocks.fill(pavementblock, positions.add(startpos, pos(0, 1, 0)), positions.add(startpos, pos(0, 1, -streetlength*2)))
+    blocks.fill(pavementblock, positions.add(startpos, pos(0, 1, streetwidth)), positions.add(startpos, pos(0, 1, streetwidth+streetlength*2)))
     startpos = positions.add(startpos, pos(streetwidth, 0, 0))
-    blocks.fill(whiteblock, startpos, positions.add(startpos, pos(-1, 0, streetwidth)))
+    blocks.fill(pavementblock, positions.add(startpos, pos(0, 1, 0)), positions.add(startpos, pos(0, 1, -streetlength*2)))
+    blocks.fill(pavementblock, positions.add(startpos, pos(0, 1, streetwidth)), positions.add(startpos, pos(0, 1, streetwidth+(streetlength*2))))
 
 
 
@@ -39,12 +54,12 @@ def createhouse(setpos, houselength):
     setblock = houseblocks[randint(0, houseblocks.length-1)]
     househeight = randint(20, 50)
     for i in range(streetlength):
-        blocks.fill(setblock, positions.add(setpos, pos(-1, 0, -1-i)), positions.add(setpos, pos(-houselength, househeight, -1-i)))
+        blocks.fill(setblock, positions.add(setpos, pos(-2, 0, -1-i)), positions.add(setpos, pos(-houselength, househeight, -1-i)))
         if(i%2 == 1 and i < streetlength - 1):
-            blocks.fill(glassblock, positions.add(setpos, pos(-1, 0, -1-i)), positions.add(setpos, pos(-houselength, househeight-2, -1-i)))
+            blocks.replace(glassblock, setblock, positions.add(setpos, pos(-1, 2, -1-i)), positions.add(setpos, pos(-houselength, househeight-2, -1-i)))
     for i in range(streetlength/2-1):
-        blocks.fill(glassblock, positions.add(setpos, pos(-2-2*i, 0, -1)), positions.add(setpos, pos(-2-2*i, househeight-2, -streetlength)))
+        blocks.replace(glassblock, setblock, positions.add(setpos, pos(-1-2*i, 2, -1)), positions.add(setpos, pos(-1-2*i, househeight-2, -streetlength)))
 
 
 
-player.on_chat("run", on_on_chat)
+player.on_chat("run", createdistrict)
