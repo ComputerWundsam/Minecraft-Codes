06339514dbb5fe71606b759a347b822a6698fe05
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-07-20 10:51:08 +0200

    Update CreateCircle.py

--- Generic Files/CreateCircle.py
+++ Generic Files/CreateCircle.py
@@ -1,1 +1,4 @@
+pi = 3.14159265359
+radius = 5
+
 blocks.fill(IRON_BLOCK, positions.add(startpos, pos(0, radius, 0)), positions.add(startpos, pos(Math.sin(i/radius)*radius, radius, Math.cos(i/radius)*radius)))
