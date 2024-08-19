b772551288cd2b896885e5a473542fee0e841e8b
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-08-19 22:24:16 +0200

    Create Torus.py

---
+++ Maths Test Files/Torus.py
@@ -0,0 +1,17 @@
+pi = 3.14159265359
+radius = 20
+iradius = 10
+diff = (radius - iradius)
+
+def on_on_chat():
+    startpos = player.position()
+    for i in range(diff*pi):
+        outradius = iradius + diff * Math.sin(i/diff)
+        inradius = iradius - diff * Math.sin(i/diff)
+        for j in range(2*outradius*pi):
+            blocks.fill(RED_CONCRETE, positions.add(startpos, pos(Math.sin(j/outradius)*outradius -1, Math.sin(i/(radius*2))*(radius*2), Math.cos(j/outradius)*outradius-1)), 
+            positions.add(startpos, pos(Math.sin(j/outradius)*outradius+1, Math.sin(i/(radius*2))*(radius*2), Math.cos(j/outradius)*outradius +1)))
+        for j in range(2*inradius*pi):
+                    blocks.fill(RED_CONCRETE, positions.add(startpos, pos(Math.sin(j/inradius)*inradius-1, Math.sin(i/(radius*2))*(radius*2), Math.cos(j/inradius)*inradius-1)), 
+                    positions.add(startpos, pos(Math.sin(j/inradius)*inradius+1, Math.sin(i/(radius*2))*(radius*2), Math.cos(j/inradius)*inradius+1)))
+player.on_chat("run", on_on_chat)
