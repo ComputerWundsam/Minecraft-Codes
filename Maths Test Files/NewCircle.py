d3fe2e8857b6cdd3870f0f55971b7e102f0fe743
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-08-20 23:45:26 +0200

    Update NewCircle.py

--- Maths Test Files/NewCircle.py
+++ Maths Test Files/NewCircle.py
@@ -6,7 +6,7 @@ circleblock = GOLD_BLOCK
 def on_on_chat():
     global startpos
     startpos = player.position()
-    for i in range(radius*2):
+    for i in range(radius*pi/2):
         blocks.fill(circleblock, positions.add(startpos, pos(-Math.sin(i/radius)*radius-1, 0, -Math.cos(i/radius)*radius-1)), 
             positions.add(startpos, pos(Math.sin(i/radius)*radius+1, 0, -Math.cos(i/radius)*radius+1)), FillOperation.REPLACE)
         blocks.fill(circleblock, positions.add(startpos, pos(-Math.sin(i/radius)*radius-1, 0, Math.cos(i/radius)*radius-1)),
