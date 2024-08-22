c7cac9c6a486a90f3073737847c4fcef39dc5f3a
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-08-22 21:56:08 +0200

    Update StoneLavaPyramid.py

--- Megabuilds/StoneLavaPyramid.py
+++ Megabuilds/StoneLavaPyramid.py
@@ -1,13 +1,13 @@
-height = 3
-layers = 8
+height = 8
+layers = 10
 startpos = pos(0, 0, 0)
 entrancepos = pos(0, 0, 0)
 
 def on_on_chat():
     global startpos
     startpos = player.position()
-    createrings()
     createpivotpoint()
+    createrings()
     blocks.place(LAVA, positions.add(startpos, pos(0, -height+1, 0)))
     waitforpivotpoint()
     blocks.place(WATER, positions.add(startpos, pos(0, -height+3, 0)))
@@ -16,13 +16,12 @@ def on_on_chat():
     createentrance()
     createfloor()
 
-
 def createrings():
     for i in range(layers):
         for j in range(i):
             blocks.fill(GLOWSTONE, positions.add(startpos, pos(i-j-1, -i*height, j)), positions.add(startpos, pos(-i+j+1, -i*height, -j)))
         for j in range(i-1):
-            blocks.fill(AIR, positions.add(startpos, pos(i-2-j, -i*height, j)), positions.add(startpos, pos(-i+2+j, -i*height, -j)))
+            player.execute("fill " + positions.add(startpos, pos(i-2-j, -i*height, j)) + " " + positions.add(startpos, pos(-i+2+j, -i*height+1, -j)) + " barrier")
 
 def createpivotpoint():
     global entrancepos
@@ -34,17 +33,27 @@ def createpivotpoint():
 
 def waitforpivotpoint():
     global entrancepos
-    loops.pause(500*height*layers)
     while blocks.test_for_block(AIR, entrancepos):
         loops.pause(500)
+    entrancepos = positions.add(entrancepos, pos(-1, 1, 0))
 
 def createentrance():
-    blocks.fill(COBBLESTONE, positions.add(entrancepos, pos(0, -1, -1)), positions.add(entrancepos, pos(0, 3, 1)))
-    blocks.place(OAK_DOOR, positions.add(entrancepos, pos(0, 1, 0)))
+    blocks.fill(COBBLESTONE, positions.add(entrancepos, pos(2, -3, -1)), positions.add(entrancepos, pos(2, 1, 1)))
+    blocks.place(OAK_DOOR, positions.add(entrancepos, pos(2, -1, 0)))
 
 def createfloor():
     for i in range(layers):
-        player.execute("fill " + positions.add(entrancepos, pos(0+i, 1, i)) + " " + positions.add(entrancepos, pos(2*layers-i, height*layers, -i)) + " air replace lava")
-        blocks.fill(DIAMOND_BLOCK, positions.add(entrancepos, pos(0+i, 0, i)), positions.add(entrancepos, pos(2*layers-i, 0, -i)))
+        blocks.fill(DIAMOND_BLOCK, positions.add(entrancepos, pos(i+2, -2, i)), positions.add(entrancepos, pos(2*layers-i, -2, -i)))
+
 
 player.on_chat("run", on_on_chat)
+
+def on_chat():
+    global entrancepos 
+    entrancepos = player.position()
+    createfloor()
+player.on_chat("jump", on_chat)
+
+# def on_travelled_walk():
+#     blocks.fill(AIR, pos(-15, 0, -15), pos(15, 10, 15))
+# player.on_travelled(FLY, on_travelled_walk)
