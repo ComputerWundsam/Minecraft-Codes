486246fbdcb2f8374116a334b7871623ce364a62
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2024-09-11 22:13:15 +0200

    Update RiverMaker.py

--- NatureWonders/RiverMaker.py
+++ NatureWonders/RiverMaker.py
@@ -1,7 +1,7 @@
 xdir = [1, 0, -1, 0]
 zdir = [0, -1, 0, 1]
-size = 3
-radius = 8
+size = 5
+radius = 16
 pi = 3.14159265359
 
 startpos = player.position()
@@ -9,38 +9,42 @@ def on_on_chat():
     c = 100
     global startpos
     startpos = positions.add(player.position(), pos(0, -1, 0))
-
-    for i in range(200):
+    for i in range(20000):
         if c <= 4:
             c += 100
-        blocks.fill(WATER, positions.add(startpos, pos(-size, 0, -size)), positions.add(startpos, pos(size, -2, size)), FillOperation.REPLACE)
+        FillWithRiver(startpos)
         startpos = positions.add(startpos, pos(xdir[c%xdir.length], 0, zdir[c%zdir.length]))
         dirs = randint(0, 20)
-        if(dirs < 2):
+        if(dirs < 5):
             startpos = positions.add(startpos, pos(xdir[(c+1)%xdir.length], 0, zdir[(c+1)%zdir.length]))
             dirs = randint(0, 20)
-            if dirs < 5:
+            if dirs < 1:
+                player.say("left")
                 c += 1
-                #createcurve(positions.add(startpos, pos(xdir[(c)%xdir.length]*radius, 0, zdir[(c)%zdir.length]*radius)), c-1, c)
-
-        elif (dirs > 18):
+                createcurve(positions.add(startpos, pos(xdir[(c)%xdir.length]*radius, 0, zdir[(c)%zdir.length]*radius)), c+2, c+3)
+                startpos = positions.add(startpos, pos((xdir[(c+3)%xdir.length] + xdir[(c+4)%xdir.length])*radius, 0, (zdir[(c+3)%zdir.length] + zdir[(c+4)%zdir.length])*radius))
+        elif (dirs > 15):
             startpos = positions.add(startpos, pos(xdir[(c-1)%xdir.length], 0, zdir[(c-1)%zdir.length]))
             dirs = randint(0, 20)
-            if dirs < 5:
-                #createcurve(positions.add(startpos, pos(xdir[(c-1)%xdir.length]*radius, 0, zdir[(c-1)%zdir.length]*radius)), c-1, c)
+            if dirs < 1:
+                player.say("right")
+                createcurve(positions.add(startpos, pos(xdir[(c-1)%xdir.length]*radius, 0, zdir[(c-1)%zdir.length]*radius)), c, c+1)
+                startpos = positions.add(startpos, pos((xdir[(c+3)%xdir.length] + xdir[(c+4)%xdir.length])*radius, 0, (zdir[(c+3)%zdir.length] + zdir[(c+4)%zdir.length])*radius))
                 c -= 1
 
-def createcurve(setpos, fc, sc, c):
+def createcurve(setpos, fc, sc):
+    global startpos
     setx = xdir[fc%xdir.length]+xdir[sc%xdir.length]
     setz = zdir[fc%zdir.length]+zdir[sc%zdir.length]
-    change = c%xdir.length*(pi/2)
-
+    change = (sc%xdir.length)*(pi/2)
     for i in range(radius*pi/2):
-        blocks.fill(WATER, positions.add(setpos, pos(Math.sin((i+change)/radius*setx)*radius-size, 0, Math.cos((i+change)/radius*setx)*radius-size)), 
-                            positions.add(setpos, pos(Math.sin((i+change)/radius*setz)*radius+size, -2, Math.cos((i+change)/radius*setz)*radius+size)), FillOperation.REPLACE)
+        FillWithRiver(positions.add(setpos, pos(Math.sin((i+change)/radius)*radius*setx, 0, Math.cos((i+change)/radius)*radius*setz)))
+
+def FillWithRiver(setpos):
+    blocks.fill(WATER, positions.add(setpos, pos(-size, 0, -size)),
+            positions.add(setpos, pos(size, -2, size)), FillOperation.REPLACE)
+    blocks.fill(AIR, positions.add(setpos, pos(-size, 1, -size)),
+            positions.add(setpos, pos(size, 200, size)), FillOperation.REPLACE)
 
 player.on_chat("run", on_on_chat)
 
-testpos = pos(0, -1, 0).to_world()
-for i in range(4):
-    createcurve(testpos, i, i+1, 12+i)
