e125e20db6292c8cde3eb46edb39b92e9d201500
ComputerWundsam <83645432+ComputerWundsam@users.noreply.github.com>
2025-05-01 22:02:57 +0200

    Create MegaSnowball.py

---
+++ commandFiles/MegaSnowball.py
@@ -0,0 +1,3 @@
+while True:
+    player.execute("execute @e[type=arrow] ^ ^ ^ fill ~-12 ~-12 ~-12 ~12 ~12 ~12 air")
+    player.execute("execute @e[type=snowball] ^ ^ ^ fill ~-5 ~-5 ~-5 ~5 ~5 ~5 air")
